32ddecc4d547b8a89dc95a56d82bcc4019dc0980
gabora93 <gabora93@gmail.com>
2019-10-08 21:09:30 -0700

    cuarto commit

--- src/mongo/mongooseClient.js
+++ src/mongo/mongooseClient.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
-let mongoURI = "mongodb+srv://gabo:123456a@cluster0-tjuco.mongodb.net/alumnos?retryWrites=true&w=majority"
 
 
-mongoose.connect(mongoURI,{useNewUrlParser : true, useUnifiedTopology: true});
+//let mongoURI = "mongodb+srv://gabo:123456a@cluster0-tjuco.mongodb.net/alumnos?retryWrites=true&w=majority"
+//mongoose.connect(mongoURI,{useNewUrlParser : true, useUnifiedTopology: true});
+
+
+let MDURI = process.env.MONGODBURI
+mongoose.connect(MDURI || dbURI, {useNewUrlParser : true});
+
+
 console.log(mongoose.connection.readyState);
 
 
