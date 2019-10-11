eb8f7b6ad90bd0b9b9df4e87484b0e97168d56e9
gabora93 <gabora93@gmail.com>
2019-10-10 19:23:30 -0700

    dburi

--- src/mongo/mongooseClient.js
+++ src/mongo/mongooseClient.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-//let mongoURI = "mongodb+srv://gabo:123456a@cluster0-tjuco.mongodb.net/alumnos?retryWrites=true&w=majority"
+let dbURI = "mongodb+srv://gabo:123456a@cluster0-tjuco.mongodb.net/alumnos?retryWrites=true&w=majority"
 //mongoose.connect(mongoURI,{useNewUrlParser : true, useUnifiedTopology: true});
 
 
